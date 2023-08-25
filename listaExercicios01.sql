e95f0b55e38b3bc1209b2e4e1c5228474f188bdf
unknown <caua2006carvalho@gmail.com>
2023-08-24 23:00:27 -0300

    Atividade 3 feita

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -1,4 +1,13 @@
 -- Atividade 1
 select titulo from livros;
+
+
 -- Atividade 2
-select nome from autores where nascimento < '1900-01-01';
+select nome from autores where nascimento < '1900-01-01';  
+
+
+-- Atividade 3
+select livros.titulo, autores.nome 
+from livros 
+inner join autores on livros.autor_id = autores.id
+where autores.nome ='J.K. Rowling'; 
