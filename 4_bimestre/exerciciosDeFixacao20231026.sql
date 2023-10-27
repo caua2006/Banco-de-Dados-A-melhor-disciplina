009e9e0b4a5fd64ffd1803e35aa6a7b66e3d3ccf
caua2006 <102489942+caua2006@users.noreply.github.com>
2023-10-26 22:26:26 -0300

    ex 1 2 3

---
+++ 4_bimestre/exerciciosDeFixacao20231026.sql
@@ -0,0 +1,21 @@
+---1---------------------------------
+create trigger after_Cliente_insert 
+ after insert on Clientes
+  FOR EACH row
+  insert into Auditoria (mensagem, data_hora) values ('Novo Cliente', now());
+
+----2-----------------------
+ create trigger before_Cliente_delete
+  before delete on Clientes
+  for each row
+  insert into Auditoria
+  set action = 'Delete',
+  nome = old.nome;
+
+-----3------------------------
+create trigger after_cliente_update
+  after update on Clientes
+  for each row
+  insert into Auditoria
+  set action = 'Update',
+  old.nome = new.nome;
